3811f326011e9b3ca0fc4404a5e866477af38299
Balamiayush <aryanbalami54@gmail.com>
2026-08-14 15:53:18 +0545

    feat: add products page with hero, content sections, and FAQ component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,6 +4,8 @@ import ResolveSection from "./_components/ResolveSection";
 import StickyProductSection from "./_components/StickyProductSection";
 import BacklogsSection from "./_components/BacklogsSection";
 import BackendByIndustrySection from "./_components/BackendByIndustrySection";
+import FAQ from "../(home-page)/_components/FAQSection";
+import StartVerifyingSection from "../(home-page)/_components/StartVerifyingSection";
 
 export default function page() {
   return (
@@ -13,6 +15,8 @@ export default function page() {
       <StickyProductSection/>
       <BacklogsSection/>
       <BackendByIndustrySection/>
+      <StartVerifyingSection/>
+      <FAQ/>
     </div>
   );
 }
