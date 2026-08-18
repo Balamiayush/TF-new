1141a4928e4f791e80823f88928ff97632d4ba51
Balamiayush <aryanbalami54@gmail.com>
2026-08-18 11:42:15 +0545

    feat: add platform page with hero, animation, and infrastructure sections

--- src/app/platform/page.tsx
+++ src/app/platform/page.tsx
@@ -20,9 +20,9 @@ export default function page() {
     <ScrollAnimationSection/>
     <ModularBlocksSection/>
          <Infrastructure />
+    <BehindObsidian/>
    <ReadAllStories/>
    <StartVerifyingSection/>
-    <BehindObsidian/>
    </div>
   )
 }
